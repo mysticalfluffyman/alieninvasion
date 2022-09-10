dcd5752b6679ff711f451a26b26ed789fb76d22b
Pratik Rajbhandari <rajbhandaripratik8@gmail.com>
2022-09-10 22:05:37 +0545

    feat: ship fires limited bullets

--- game_functions.py
+++ game_functions.py
@@ -1,33 +1,51 @@
 import sys
 import pygame
+from bullet import Bullet
 
-def check_events(ship):
+def check_events(ai_settings, screen, ship, bullets):
     for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                check_keydown_event(event, ship)
+                check_keydown_event(event, ai_settings, screen, ship, bullets)
             elif event.type == pygame.KEYUP:
-                check_keydown_event(event, ship)
+                check_keyup_event(event, ship)
 
-def check_keydown_event(event, ship):
+def check_keydown_event(event, ai_settings, screen, ship, bullets):
     if event.key == pygame.K_RIGHT:
         #move the ship to right
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         #move the ship to right
         ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        fire_bullets(ai_settings,screen,ship,bullets)
+
+def fire_bullets(ai_settings, screen, ship, bullets):
+    # Create a new bullet and add it to the bullets groups.
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
 def check_keyup_event(event, ship):
     if event.key == pygame.K_RIGHT:
-                    #move the ship to right
+        #move the ship to right
         ship.moving_right = False
     elif event.key == pygame.K_LEFT:
-                    #move the ship to right
+        #move the ship to right
         ship.moving_left = False
 
-def update_screen(ai_settings, screen, ship):
+def update_screen(ai_settings, screen, ship, bullets):
     """Update images on the screen and flip to the new screen"""
     screen.fill(ai_settings.bg_color)
     ship.blitme()
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     pygame.display.flip()
+    
+def update_bullets(bullets):
+    bullets.update()
+    # Delete bullets
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
